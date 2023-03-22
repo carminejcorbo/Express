61a60ad5168497348e16e9b5a755f2dcdbd5c86b
Carmine Corbo <carmine.j.corbo@gmail.com>
2023-03-22 10:46:30 -0400

    Change formatting

--- client/src/components/SectionWrapper.js
+++ client/src/components/SectionWrapper.js
@@ -3,34 +3,24 @@ import { StyledSection } from '../styles';
 
 const SectionWrapper = ({ children, title, seeAllLink, breadcrumb }) => {
   let breadcrumbContent = null;
+  let titleContent = null;
+  let seeAllLinkContent = null;
+
   if (breadcrumb) {
     breadcrumbContent = (
-      <span className="section__breadcrumb">
-        <Link to="/">Profile</Link>
-      </span>
+      <span className="section__breadcrumb">  <Link to="/">Profile</Link>  </span>
     );
   }
-
-  let titleContent = null;
   if (title) {
     if (seeAllLink) {
-      titleContent = (
-        <Link to={seeAllLink}>{title}</Link>
-      );
+      titleContent = ( <Link to={seeAllLink}>{title}</Link> );
     } else {
-      titleContent = (
-        <span>{title}</span>
-      );
+      titleContent = ( <span>{title}</span>);
     }
   }
-
-  let seeAllLinkContent = null;
   if (seeAllLink) {
-    seeAllLinkContent = (
-      <Link to={seeAllLink} className="section__see-all">See All</Link>
-    );
+    seeAllLinkContent = ( <Link to={seeAllLink} className="section__see-all">See All</Link> );
   }
-
   return (
     <StyledSection>
       <div className="section__inner">
