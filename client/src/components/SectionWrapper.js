7375ea80ca417c62037026418c66837902daeb9d
Carmine Corbo <carmine.j.corbo@gmail.com>
2023-03-22 10:23:28 -0400

    I refactored this to be more readable.

--- client/src/components/SectionWrapper.js
+++ client/src/components/SectionWrapper.js
@@ -1,34 +1,50 @@
 import { Link } from 'react-router-dom';
 import { StyledSection } from '../styles';
 
-const SectionWrapper = ({ children, title, seeAllLink, breadcrumb }) => (
-  <StyledSection>
-    <div className="section__inner">
-      <div className="section__top">
-        <h2 className="section__heading">
-          {breadcrumb && (
-            <span className="section__breadcrumb">
-              <Link to="/">Profile</Link>
-            </span>
-          )}
-          {title && (
-            <>
-              {seeAllLink ? (
-                <Link to={seeAllLink}>{title}</Link>
-              ) : (
-                <span>{title}</span>
-              )}
-            </>
-          )}
-        </h2>
-        {seeAllLink && (
-          <Link to={seeAllLink} className="section__see-all">See All</Link>
-        )}
-      </div>
+const SectionWrapper = ({ children, title, seeAllLink, breadcrumb }) => {
+  let breadcrumbContent = null;
+  if (breadcrumb) {
+    breadcrumbContent = (
+      <span className="section__breadcrumb">
+        <Link to="/">Profile</Link>
+      </span>
+    );
+  }
 
-      {children}
-    </div>
-  </StyledSection>
-);
+  let titleContent = null;
+  if (title) {
+    if (seeAllLink) {
+      titleContent = (
+        <Link to={seeAllLink}>{title}</Link>
+      );
+    } else {
+      titleContent = (
+        <span>{title}</span>
+      );
+    }
+  }
+
+  let seeAllLinkContent = null;
+  if (seeAllLink) {
+    seeAllLinkContent = (
+      <Link to={seeAllLink} className="section__see-all">See All</Link>
+    );
+  }
+
+  return (
+    <StyledSection>
+      <div className="section__inner">
+        <div className="section__top">
+          <h2 className="section__heading">
+            {breadcrumbContent}
+            {titleContent}
+          </h2>
+          {seeAllLinkContent}
+        </div>
+        {children}
+      </div>
+    </StyledSection>
+  );
+}
 
 export default SectionWrapper;
